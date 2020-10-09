7285f25e351427b2e7fae5e01e5cdf3b8f82a979
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-10-09 18:17:21 +0200

    Add prototype for new version from Edictor

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_forms(cldf_dataset):
-    assert len(list(cldf_dataset["FormTable"])) == 4546
-    assert any(f["Form"] == "sɛ̃⁵⁵tsɿ³³tɯ²¹" for f in cldf_dataset["FormTable"])
+    assert len(list(cldf_dataset["FormTable"])) == 20402
+    assert any(f["Form"] == "tʃade" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
-    assert len(list(cldf_dataset["ParameterTable"])) == 499
+    assert len(list(cldf_dataset["ParameterTable"])) == 407
 
 
 def test_languages(cldf_dataset):
-    assert len(list(cldf_dataset["LanguageTable"])) == 9
+    assert len(list(cldf_dataset["LanguageTable"])) == 76
