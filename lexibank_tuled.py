b25cd3ff69d132f1d942689d6f697d8f398abde9
LinguList <mattis.list@posteo.de>
2020-10-28 11:08:35 +0100

    update

--- lexibank_tuled.py
+++ lexibank_tuled.py
@@ -1,10 +1,9 @@
 import attr
 from pathlib import Path
 
-from pylexibank import Concept, Language, FormSpec
+from pylexibank import Concept, Language, Cognate
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank.util import progressbar
-from cldfbench import CLDFSpec
 from csvw import Datatype
 from pyclts import CLTS
 
@@ -25,22 +24,34 @@ class CustomLanguage(Language):
     SubGroup = attr.ib(default=None)
     Source = attr.ib(default=None)
 
+@attr.s
+class CustomCognate(Cognate):
+    Segment_Slice = attr.ib(default=None)
+
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "tuled"
     concept_class = CustomConcept
     language_class = CustomLanguage
-    form_spec = FormSpec(
-            missing_data=("?", ),
-            strip_inside_brackets=True,
-            separators = ",/",
-            brackets={"{": "}", "(": ")", "[": "]"}
+    cognate_class = CustomCognate
+    
+
+    def cmd_download(self, args):
+        print('download')
+        self.raw_dir.download(
+            "https://lingulist.de/edictor/triples/get_data.py?file=tuled&remote_dbase=tuled.sqlite3",
+            "tuled.tsv"
             )
 
     def cmd_makecldf(self, args):
 
         args.writer.add_sources()
+        args.writer["FormTable", "Segments"].separator = " + "
+        args.writer["FormTable", "Segments"].datatype = Datatype.fromvalue(
+            {"base": "string", "format": "([\\S]+)( [\\S]+)*"}
+            )
+
         concepts = {}
         for concept in self.concepts:
             idx = '{0}_{1}'.format(concept['NUMBER'], slug(concept['ENGLISH']))
@@ -51,50 +62,83 @@ class Dataset(BaseDataset):
                     Concepticon_ID=concept['CONCEPTICON_ID'],
                     #Concepticon_Gloss=concept['CONCEPTICON_GLOSS']
                     )
-            concepts[concept['PORTUGUESE']] = idx
+            concepts[concept['ENGLISH']] = idx
         languages = {}
+        errors, blacklist = set(), set()
         for row in self.languages:
-            args.writer.add_language(
-                    ID=row['ID'],
-                    Name=row['Name'],
-                    SubGroup=row['SubGroup'],
-                    #Latitude=row['Latitude'],
-                    #Longitude=row['Longitude'],
-                    Glottocode=row['Glottocode'],
-                    )
-            languages[row['Name']] = row['ID']
-        
-        data = [row for row in self.raw_dir.read_csv('spreadsheet.tsv',
-            delimiter='\t', dicts=False)]
-        header = data[2]
-        missc, missl = set(), set()
-        for row in data[5:]:
-            language = row[0]
-            lid = languages.get(language)
-            if lid:
-                print(lid)
-                for i in range(7, len(row)-3, 3):
-                    if i < len(row)+1:
-                        concept=data[1][i]
-                        cogid=row[i+1]
-                        note=row[i+2]
-                        form=row[i]
-                        if concept in concepts and form.strip():
-                            for lex in args.writer.add_forms_from_value(
-                                    Parameter_ID=concepts[concept],
-                                    Language_ID=lid,
-                                    Value=form,
-                                    Source=''
-                                    ):
-                                args.writer.add_cognate(
-                                        lexeme=lex,
-                                        Cognateset_ID=slug('{0}-{1}'.format(concept,
-                                            cogid)),
-                                        )
-                        else:
-                            missc.add(concept)
+            if not -180 < float(row['Latitude']) < 180:
+                errors.add('LATITUDE {0}'.format(row['Name']))
+            elif not -180 < float(row['Longitude']) < 180:
+                errors.add('LONGITUDE {0}'.format(row['Name']))
             else:
-                missl.add(language)
+                args.writer.add_language(
+                        ID=row['ID'],
+                        Name=row['Name'],
+                        SubGroup=row['SubGroup'],
+                        Latitude=row['Longitude'],
+                        Longitude=row['Latitude'],
+                        Glottocode=row['Glottocode'],
+                        )
+                languages[row['Name']] = row['ID']
+        wl = lingpy.Wordlist(self.raw_dir.joinpath('tuled.tsv').as_posix())
+        bipa = CLTS(args.clts.dir).bipa
+        for idx, tokens, glosses, cogids, alignment in wl.iter_rows(
+                'tokens', 'morphemes', 'cogids', 'alignment'):
+            tl, gl, cl, al = (
+                    len(lingpy.basictypes.lists(tokens).n),
+                    len(glosses),
+                    len(cogids),
+                    len(lingpy.basictypes.lists(alignment).n)
+                    )
+            if tl != gl or tl != cl or gl != cl or al != gl or al != cl:
+                errors.add('LENGTH: {0} {1} {2}'.format(
+                    idx,
+                    wl[idx, 'language'],
+                    wl[idx, 'concept']))
+                blacklist.add(idx)
+            for token in tokens:
+                if bipa[token].type == 'unknownsound':
+                    errors.add('SOUND: {0}'.format(token))
+                    blacklist.add(idx)
 
-                
+        visited = set()
+        for idx in wl:
+            if wl[idx, 'concept'] not in concepts:
+                if wl[idx, 'concept'] not in visited:
+                    args.log.warn('Missing concept {0}'.format(wl[idx,
+                    'concept']))
+                    visited.add(wl[idx, 'concept'])
+                    errors.add('CONCEPT {0}'.format(wl[idx, 'concept']))
+            elif wl[idx, 'doculect'] not in languages:
+                if wl[idx, 'doculect'] not in visited:
+                    args.log.warn("Missing language {0}".format(wl[idx, 'doculect']
+                        ))
+                    visited.add(wl[idx, 'doculect'])
+                    errors.add('LANGUAGE {0}'.format(wl[idx, 'doculect']))
+            else:
+                if ''.join(wl[idx, 'tokens']).strip() and idx not in blacklist:
+                    lex = args.writer.add_form_with_segments(
+                            Language_ID=wl[idx, 'doculect'],
+                            Parameter_ID=concepts[wl[idx, 'concept']],
+                            Value=wl[idx, 'value'] or ''.join(wl[idx, 'tokens']),
+                            Form=wl[idx, 'form'] or ''.join(wl[idx, 'tokens']),
+                            Segments=wl[idx, 'tokens']
+                            )
+                    for gloss_index, cogid in enumerate(wl[idx, 'cogids']):
+                        alignment = lingpy.basictypes.lists(
+                                wl[idx, "alignment"]).n[gloss_index]
+                        args.writer.add_cognate(
+                                lexeme=lex,
+                                Cognateset_ID=cogid,
+                                Segment_Slice=gloss_index + 1,
+                                Alignment=alignment
+                                )
+                else:
+                    args.log.warn('Entry ID={0}, concept={1}, language={2} is empty'.format(
+                        idx, wl[idx, 'concept'], wl[idx, 'doculect']))
+        
 
+        with open(self.dir.joinpath('errors.md'), 'w') as f:
+            f.write('# Error Analysis for TULED\n')
+            for error in sorted(errors):
+                f.write('* '+error)
