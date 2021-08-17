c092bc24f38a20e35c5f7e7f37ab7358ee0800b6
lingulist <mattis.list@lingulist.de>
2021-08-17 21:08:51 +0200

    update new files

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_forms(cldf_dataset):
-    assert len(list(cldf_dataset["FormTable"])) > 222501
+    assert len(list(cldf_dataset["FormTable"])) > 22500
 
 
 def test_parameters(cldf_dataset):
